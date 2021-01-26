2a04b46eabd18fdfa77e9d2c2654d12b14920f68
AndrewTiu8 <mt1229@georgetown.edu>
2021-01-26 17:43:03 -0500

    Attempting to fix whitespace below map

--- COVID_dash_app.py
+++ COVID_dash_app.py
@@ -400,7 +400,7 @@ row_2 = dbc.Row(
                         html.Div(dcc.Graph(
                             id='US-choropleth',
                             figure=US_choropleth(),
-                            style={'height': '100%'},  # seems to control graph height within the row?
+                            style={'marginBottom': 0},  # can control graph height within the row?
                             config={'modeBarButtonsToRemove': ['select2d',
                                                                'lasso2d',
                                                                'zoom2d',
